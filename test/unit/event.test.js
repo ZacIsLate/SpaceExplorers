d31a96abd05b9174198d4df6d304f2a9028475be
Shane Moyo <smoyo@pdx.edu>
2017-10-30 11:42:02 -0700

    pass saves an enemy with an id test

--- test/unit/event.test.js
+++ test/unit/event.test.js
@@ -8,7 +8,7 @@ describe('Event model', ()=>{
          
         const event = new Event({
             scenario: 'You have encountered an Advanced Cylon War Raider Battalion inside of an astroid field!',
-            enviornment: '590643bc2cd3da2808b0e651', 
+            environment: '590643bc2cd3da2808b0e651', 
             enemy: '590643bc2cd3da2808b0e651',
             actions: [
                 {
@@ -32,7 +32,7 @@ describe('Event model', ()=>{
         const event = new Event({});
         const { errors } = event.validateSync();
         assert.equal(errors.scenario.kind, 'required');
-        assert.equal(errors.enviornment.kind, 'required');
+        assert.equal(errors.environment.kind, 'required');
         assert.equal(errors.enemy.kind, 'required');
     });
 });
