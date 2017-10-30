7b6442bdf0a9fc131cb6159a72e23d66f8eea10b
Zac-L <Zachlichens@gmail.com>
2017-10-30 11:05:38 -0700

    created green test for get ships method

--- test/e2e/ships.test.js
+++ test/e2e/ships.test.js
@@ -11,19 +11,52 @@ describe('Ship CRUD', () => {
         shipData = [
             {
                 name: 'Moya',
-                hp: 1000,
-                dmg: 100,
+                hp: 1500,
+                dmg: 50,
                 description: 'A living sentient bio-mechanical space ship.',
                 class: 'Leviathan'
+            },
+            {
+                name: 'Raza',
+                hp: 900,
+                dmg: 200,
+                description: 'The Raza is a faster-than-light armed space transport equipped with various countermeasures such as vector thrust capable nacelles. The interiors feature a spacious bridge, a small mess hall, and private crew quarters for at least six crew members. Gravity aboard the Raza is artificially generated.',
+                class: 'StarShip'
             }
         ];
     });
 
-    describe.only('POST ship', () => {
+    describe('POST ship', () => {
         it('returns a ship with a new id', () => {
             return request.post('/api/ships')
                 .send(shipData[0])
                 .then(res => assert.ok(res.body._id));
+        });
+    });
+
+    describe('GET Ship', () => {
+        it('returns all when no id given', () => {
+            const savedShips = [
+                request.post('/api/ships')
+                    .send(shipData[0]),
+                request.post('/api/ships')
+                    .send(shipData[1])
+            ];
+
+            return Promise.all(savedShips)
+                .then(resArray => {
+                    resArray = resArray.map(res => {
+                        return {
+                            name: res.body.name,
+                            _id: res.body._id
+                        };
+                    });
+                    return request.get('/api/ships')
+                        .then(received => {
+                            assert.deepInclude(received.body, resArray[0]);
+                            assert.deepInclude(received.body, resArray[1]);
+                        });
+                });
         });
     });
 });
