a7a31053cb3d29c7bcde28f3d3eab407926df2b9
Shane Moyo <smoyo@pdx.edu>
2017-10-30 09:43:56 -0700

    add failing enemy model schema test

---
+++ test/unit/enemy.model.js
@@ -0,0 +1,15 @@
+const assert = require('chai').assert;
+const Enemy = require('../../lib/models/enemy');
+
+
+describe('Enemy model', ()=>{
+
+    it('Validates a good enemy model', () => {
+        const enemy = new Enemy({
+            name: 'Advanced Cylon War Raider Battalion',
+            AttackPower: 25,
+            HealthPoints: 55
+        });
+        assert.equal(enemy.validateSync(), undefined);
+    });
+});
