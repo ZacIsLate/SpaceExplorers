72618cd405598af6c7e603a48c7b51e4b2e6fbba
Zac-L <Zachlichens@gmail.com>
2017-10-30 09:33:48 -0700

    updated package to include chai, chai-http

---
+++ test/unit/Ship.test.js
@@ -0,0 +1,22 @@
+const { assert } = require('chai');
+const Ship = require('../../lib/models/Ship');
+
+describe('Ship Model', () => {
+    let shipData = null;
+
+    beforeEach(() => shipData = {
+        name: 'Moya',
+        hp: 1000,
+        dmg: 100,
+        description: 'A living sentient bio-mechanical space ship.',
+        class: 'Leviathan'
+    });
+
+    it('Valid Model', () => {
+        const farScape = new Ship(shipData);
+
+        assert.ok(!farScape.validateSync());
+        assert.ok(farScape.name);
+    });
+    
+});
