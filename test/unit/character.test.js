158656e3bedfb5441bb41c4980b46337d3af9c86
Shane Moyo <smoyo@pdx.edu>
2017-10-31 06:05:10 -0700

    adjusting character schema test

--- test/unit/character.test.js
+++ test/unit/character.test.js
@@ -3,11 +3,17 @@ const Character = require('../../lib/models/character');
 
 describe( 'Character model', () => {
     
-    it.only('check if good model', () => {
+    it('check if good model', () => {
         const character = new Character({
             name: 'Ellen Ripley',
             description: 'Warrant officer who survived multiple encounters with Xenomorphs',
-            ship:'590643bc2cd3da2808b0e651',
+            ship:{
+                name: 'Moya',
+                hp: 1000,
+                dmg: 100,
+                description: 'A living sentient bio-mechanical space ship.',
+                class: 'Leviathan'
+            },
             user: '590643bc2cd3da2808b0e651'
         });
         assert.equal(character.validateSync(), undefined);
