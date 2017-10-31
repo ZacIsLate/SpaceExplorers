1f0bc6e39f54eacf9e492f5896ed04075ba31e23
mugworks <greenwood.pdx@gmail.com>
2017-10-31 10:50:24 -0700

    add all files

--- test/e2e/characters.test.js
+++ test/e2e/characters.test.js
@@ -7,27 +7,33 @@ describe('Characters API', () => {
     let characterData = [];
     beforeEach(() => db.drop());
 
+    const shipData ={
+        name: 'Moya',
+        hp: 1000,
+        dmg: 100,
+        description: 'A living sentient bio-mechanical space ship.',
+        class: 'Leviathan'
+    };
+
     characterData = [
         {
             name: 'Ford Prefect',
             description: 'human/alien travel writer',
             user:'590643bc2cd3da2808b0e651',
-            ship:'590643bc2cd3da2808b0e651'
+            ship: shipData
         }, 
         {
             name: 'Mark Watney',
             description: 'Maritian - colonized a planet on his own',
             user:'590643bc2cd3da2808b0e651',
-            ship:'590643bc2cd3da2808b0e651'
+            ship: shipData
         }
     ];
 
     it('saves a character', () => {
         return request.post('/api/characters')
             .send(characterData[0])
-            .then(({ body }) => {
-                assert.equal(body.name, characterData[0].name);
-            });
+            .then(({ body }) => assert.equal(body.name, characterData[0].name));
     }),
 
     it('gets all characters', () => {
@@ -60,9 +66,30 @@ describe('Characters API', () => {
                 return request.get(`/api/characters/${savedCharacter._id}`);
             
             })
-            .then(res => {
-                assert.equal(res.body.name, 'Ford Prefect');
-            });
+            .then(res => assert.equal(res.body.name, 'Ford Prefect'));
     });
 
+    it('Should update a character', ()=>{
+        let savedCharacter = null; 
+        return request.post('/api/characters')
+            .send(characterData[0])
+            .then( res => {
+                savedCharacter = res.body;
+                characterData[0].name = 'Helena Cain';
+                return request.put(`/api/characters/${savedCharacter._id}`)
+                    .send(characterData[0]);
+            })
+            .then(res => assert.deepEqual(res.body.nModified === 1, true));
+    });
+
+    it('Deletes Character by ID', () =>{
+        let savedCharacter = null;
+        return request.post('/api/characters')
+            .send(characterData[0])
+            .then(res => {
+                savedCharacter = res.body;
+                return request.delete(`/api/characters/${savedCharacter._id}`);
+            })
+            .then( res => assert.deepEqual(res.body, { removed: true}));        
+    });
 });
