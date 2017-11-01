34e80463e029251ed70daa39e1f369149a1fbe3d
Shane Moyo <smoyo@pdx.edu>
2017-11-01 07:19:03 -0700

    refactor tests

--- test/e2e/interface.test.js
+++ test/e2e/interface.test.js
@@ -4,15 +4,17 @@ const Interface = require('../../lib/routes/interface');
 
 describe.only('Interface test',() => {
 
-   
+    const question = {
+        type: 'input',
+        name: 'name',
+        message: 'Hi, What is your name?'
+    };
 
-    it.only('should write multiple questions to command line and receive a response', () => {
+
+    
+    
+    it('should write multiple questions to command line and receive a response', () => {
         const interface = new Interface();
-        const question = {
-            type: 'input',
-            name: 'name',
-            message: 'Hi, What is your name?'
-        };
         const promise = interface.askQuestions(question)
             .then((res) => {
                 assert.ok(res);
