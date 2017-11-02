ea38ffb4a837e89218b94cfd0df5766115afc121
Zac-L <Zachlichens@gmail.com>
2017-11-02 12:30:26 -0700

    wip, working on changes to actual routes

--- cli/index.js
+++ cli/index.js
@@ -1,5 +1,6 @@
 const Game = require('./game');
 const ships = require('../lib/data/ships');
+const request = require('../test/e2e/request');
 
 const savedCharacters = [{ _id: '122', name: 'joe' }, { _id: '789', name: 'dan' }, { _id: '345', name: 'sam' }];
 const spaceEvents = [
@@ -106,34 +107,49 @@ const spaceEvents = [
 ];
 let counter = -1;
 let newEvent;
+let token = '';
 
 const service = {
     signUp(info) {
+        return request.post('/api/signup')
+            .send(info)
+            .then(({body: infoRes }) => infoRes);
         //Should post a new user and return to game object with id and token property.
-        console.log('Guest would like to create an account with: ', info);
-        return Promise.resolve({ _id: '123333', token: 'abc' });
+        // console.log('Guest would like to create an account with: ', info);
+        // return Promise.resolve({ _id: '123333', token: 'abc' });
     },
     signIn(info) {
+        return request.post('/api/signin')
+            .set('Authorization', token)
+            .send(info)
+            .then(({ body: infoRes}) => infoRes);
         //Should get a user from the database and return an object to the id and token property.
-        console.log('Guest would like to sign in with: ', info);
-        return Promise.resolve({ _id: '123', token: 'abc' });
+        // console.log('Guest would like to sign in with: ', info);
+        // return Promise.resolve({ _id: '123', token: 'abc' });
     },
     getShips(){
-        return Promise.resolve(ships.map( ship => {
-            ship._id = `123${ship.name}122345`;
-            return ship;
-        }));
+        return request.get('/api/ships')
+            .then( res => res.body );
+        // return Promise.resolve(ships.map( ship => {
+        //     ship._id = `123${ship.name}122345`;
+        //     return ship;
+        // }));
     },
     saveCharacter(characterData){
-        console.log(characterData, 'saved!');
-        const newCharacter = { _id: '445533', name: characterData['character name'] };
-        savedCharacters.push(newCharacter);
-        return Promise.resolve(characterData);
+        return request.post('/api/newchars')
+            .send(characterData)
+            .then(({ body: charId}) => charId);
+        // console.log(characterData, 'saved!');
+        // const newCharacter = { _id: '445533', name: characterData['character name'] };
+        // savedCharacters.push(newCharacter);
+        // return Promise.resolve(characterData);
     },
     getCharacters(userId) {
+        return request.get(`/api/characters/${userId._id}`)
+            .then()
         //Should return to the game an array of objects containing all characters corresponding to the userId.
-        console.log('Characters stored under this user: ', userId);
-        return Promise.resolve(savedCharacters);
+        // console.log('Characters stored under this user: ', userId);
+        // return Promise.resolve(savedCharacters);
     },
     loadEvent() {
         //Should return to a random event description object to the game.
