f6b36d9737bcf4067be5e7e5605d74729ec9ecae
Maryussuperboy <martsyalis@gmail.com>
2017-11-02 13:03:46 -0700

    half

--- test/e2e/auth.test.js
+++ test/e2e/auth.test.js
@@ -6,12 +6,40 @@ describe('Authentication API', () => {
 
     beforeEach( () =>db.drop());
     let token = null;
+    let char = null;
+    let userChar = null;
+    beforeEach( () => {
+        const ship = {
+            name: 'Moya',
+            healthPoints: 1000,
+            damage: 100,
+            description: 'A living sentient bio-mechanical space ship.',
+            class: 'Leviathan'
+        };
+
+        return request.post('/api/characters')
+            .send({
+                name: 'Ford Prefect',
+                description: 'human/alien travel writer',
+                user:'590643bc2cd3da2808b0e651',
+                ship: ship,
+                template:true
+            })
+            .then( ({body}) => char = body );
+    });
 
     beforeEach( ()=> {
         return request.post('/api/auth/signup')
             .send({name: 'Tester', password: '007'})
             .then( ({body}) => {
                 token = body.token;
+            })
+            .then( ()=>{
+                return request.post(`/api/newChar/${char._id}`)
+                    .set('Authorization', token)
+                    .then( (got) => {
+                        userChar =got;
+                    });
             });
     });
 
@@ -41,7 +69,7 @@ describe('Authentication API', () => {
             );
     });
 
-    it('should sign in with the same credentials',() => {
+    it.only('should sign in with the same credentials',() => {
         return request.post('/api/auth/signin')
             .send({ name:'Tester', password:'007'})
             .then( ({ body }) => assert.ok(body.token));
