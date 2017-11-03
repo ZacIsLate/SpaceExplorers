d7ac9d09d36e27c852cdbb644c78e0988234bdd4
Maryussuperboy <martsyalis@gmail.com>
2017-11-02 21:43:06 -0700

    changed attack to Attack becasue fuck standards

--- cli/index.js
+++ cli/index.js
@@ -57,8 +57,9 @@ const service = {
         //Should return to a random event description object to the game.
     },
     resolveAction(option) {
-        console.log('we are hitting resolve action with option of:', option.char_id.charId );
+        console.log('we are hitting resolve action with option of:', option );
         return request.post(`${API_URL}/game/character/${option.char_id.charId}/actions`)
+            .send(option)
             .then( ({body}) => {
                 console.log('we got this from post actions:', body);
                 return body;
